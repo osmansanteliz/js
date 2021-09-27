7672e4f9dc22c25b722b3de2ed60919bc6e81691
Osman Santeliz <osanteliz88@gmail.com>
2021-09-27 11:00:20 -0600

    Reparar codigo en app.js para que funcione como deberia al presionar el boton  * Se modifico codigo y se mando a llamar a la funcion.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 function imprimir() {
-    for (var i = 0; i< 8000; i++){
-        console.log(imprimio);
+    for (var i = 0; i< 3; i++){
+        console.log(imprimir);
     }
 }
 
-function presionoClickc(){
-    console.log("Click en boton")
+function presionoClick(){
+    console.log("Click en boton");
+    alert("Se dio un click en el boton");
 }
 
 imprimir();
+presionoClick();
 
 //javascript no es asincrono
