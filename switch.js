cdf777f44f9e2aa02fd090957e4bf7a6535dd387
Osman Santeliz <osanteliz88@gmail.com>
2021-10-04 10:02:20 -0600

    osanteliz/switch  * En esta rama se estudio el ciclo switch en javascript.

---
+++ switch.js
@@ -0,0 +1,12 @@
+var mes = 2;
+
+switch (mes) {
+  case 1:
+    console.log("Enero");
+    break;
+  case 2:
+    console.log("Febrero");
+    break;
+  default:
+    console.log("En caso de no cumplir ninguna de las anteriores");
+}
